7ef7c04604e32569999fc487b115764bf469981b
spone12 <spone185@gmail.com>
2020-08-02 13:36:34 +0300

    search

--- public/resource/js/search.js
+++ public/resource/js/search.js
@@ -1,7 +1,17 @@
-
+   $( document ).ready(function() 
+   {
+      $id =  $(document).find('.search-by__selected').attr('id');
+      search_by_val($id);
+   });
+  
   function change_search_by(id)
   {
      $('*').removeClass('search-by__selected');
      $('#' + id).addClass('search-by__selected');
+     $('#search-by').val(id);
+  }
+
+  function search_by_val(id)
+  {
      $('#search-by').val(id);
   }
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -6,21 +6,33 @@
     <script src="{{ asset('resource/js/search.js') }}"></script>
 @endpush
 
-<div class='container'>
-    <div class='row justify-content-start'>
+<div class='container search'>
+    <div class='row justify-content-start search-head'>
             <div class='col-sm-6 search-by'>Поиск по:
                 <div class='col-sm-4 search-by__user'>
-                    <a id='search-by__user' onclick='change_search_by(this.id);'>Пользователям</a>
+                    <a id='search-by__user' 
+
+                    @if($result->view == 1)
+                     class='search-by__selected'
+                    @endif
+
+                     onclick='change_search_by(this.id);'>Пользователям</a>
                 </div>
                 <div class='col-sm-3 search-by__notation'>
-                    <a id='search-by__notation' onclick='change_search_by(this.id);'>Новостям</a>
+                    <a id='search-by__notation'
+                    
+                    @if($result->view == 2)
+                     class='search-by__selected'
+                    @endif
+
+                     onclick='change_search_by(this.id);'>Новостям</a>
                 </div>
             </div>
             
     </div>
 
-    <div class='row'>
-     <div class='mx-auto'>
+    <div class='row justify-content-center search-body mt-2'>
+     <div class='col-5 mt-2'>
 
         @if($result->total() > 10)
             <div class='row'>
@@ -40,17 +52,23 @@
            
         @forelse($result as $k => $v)
            
-            <p>
-                {{$loop->iteration}} 
-                @if($result->view == 1)
-                    <a class='a_header' href="{{route('profile_id', $v->id) }}" title='Перейти в профиль'>{{$v->name}}</a>
-                @else
-                    <a class='a_header' href="{{route ('notation_view_id', $v->notation_id) }}">{{$v->name_notation}}</a>
-                @endif
-            </p>
+           <div class='row'>
+                <div class='col-12 mt-2 row'>
+                    <div class='col-2 col-md-1'>{{$loop->iteration}}. </div>
+                    <div class='col-9'>
+                    @if($result->view == 1)
+                        <a class='a_search' href="{{route('profile_id', $v->id) }}" title='Перейти в профиль'>{{$v->name}}</a>
+                    @else
+                        <a class='a_search' href="{{route ('notation_view_id', $v->notation_id) }}">{{$v->name_notation}}</a>
+                    @endif
+                    </div>
+                </div>
+            </div>
 
             @if($loop->last)
-                <div>Всего результатов: {{$loop->count}}</div>
+            <div class='row mt-3 mb-2'>
+                <div class='col-sm-12'>Всего результатов: <b>{{$result->total()}}</b></div>
+            </div>
             @endif
 
            
