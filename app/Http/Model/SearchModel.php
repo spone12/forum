e772972816fa730b1bd5a2b0372f5d581c7325e5
spone12 <spone185@gmail.com>
2020-08-04 11:16:34 +0300

    Вёрстка поиска

--- app/Http/Model/SearchModel.php
+++ app/Http/Model/SearchModel.php
@@ -22,11 +22,11 @@ class SearchModel extends Model
 
     protected static function search_by_notation($query)
     {
-        $result = NotationModel::select('notation_id', 'name_notation', 'rating')
+        $result = NotationModel::select('notation_id', 'name_notation', 'rating','text_notation')
             ->where('name_notation', 'LIKE', $query->search.'%')
             ->orWhere('text_notation', 'LIKE', '%'.$query->search.'%')
                         ->orderBy('notation_add_date', 'ASC')->paginate(10)->onEachSide(1);
-        
+
         $result->view = 2;
         $result->search = $query->search;
         return $result;
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -7,8 +7,8 @@
 @endpush
 
 <div class='container search'>
-    <div class='row justify-content-start search-head'>
-            <div class='col-sm-6 search-by'>Поиск по:
+    <div class='row justify-content-center search-body mt-2'>
+        <div class='col-4 col-md-3 col-lg-2 search-by'>Поиск по:
                 <div class='col-sm-4 search-by__user'>
                     <a id='search-by__user' 
 
@@ -27,29 +27,16 @@
 
                      onclick='change_search_by(this.id);'>Новостям</a>
                 </div>
-            </div>
-            
-    </div>
 
-    <div class='row justify-content-center search-body mt-2'>
+                @if($result->total() > 10)
+                <div class='row mt-3 mb-2'>
+                    <div class='col-sm-12'>Всего результатов: <b>{{$result->total()}}</b></div>
+                </div>
+                @endif
+            </div>
+
      <div class='col-5 mt-2'>
 
-        @if($result->total() > 10)
-            <div class='row'>
-                <div class='mx-auto'>
-                    @if($result->view == 1)
-                        {{ $result->appends(['search-by' => 'search-by__user',
-                                             'search' => $result->search])
-                                  ->links() }}
-                    @else
-                        {{ $result->appends(['search-by' => 'search-by__notation',
-                                             'search' => $result->search])
-                                  ->links() }}
-                    @endif
-                </div>
-            </div>
-        @endif
-           
         @forelse($result as $k => $v)
            
            <div class='row'>
@@ -64,19 +51,29 @@
                     </div>
                 </div>
             </div>
-
-            @if($loop->last)
-            <div class='row mt-3 mb-2'>
-                <div class='col-sm-12'>Всего результатов: <b>{{$result->total()}}</b></div>
-            </div>
-            @endif
-
            
         @empty
             <p>Результаты не найдены</p>
         @endforelse
      </div>
+     @if($result->total() > 10)
+            <div class='row mt-2'>
+                <div class='col col-md-10 mx-auto'>
+                    @if($result->view == 1)
+                        {{ $result->appends(['search-by' => 'search-by__user',
+                                             'search' => $result->search])
+                                  ->links() }}
+                    @else
+                        {{ $result->appends(['search-by' => 'search-by__notation',
+                                             'search' => $result->search])
+                                  ->links() }}
+                    @endif
+                </div>
+            </div>
+        @endif
     </div>
+
+   
 </div>
 
 @endsection
