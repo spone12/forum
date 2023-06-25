7c07232a52bf0c4b3ed6a01e99d5b4d11720675d
spone12 <spone185@gmail.com>
2023-06-25 10:17:18 +0300

    Fixed duplication of news on the home page

--- app/Repository/HomeRepository.php
+++ app/Repository/HomeRepository.php
@@ -17,12 +17,20 @@ class HomeRepository
     public function takeNotations() {
 
         return DB::table('notations AS n')
-            ->select('n.notation_id', 'n.user_id',
-                'n.name_notation', 'n.text_notation',
-                'users.name','users.avatar','n.notation_add_date as date_n',
-                'nv.counter_views', 'n.rating')
+            ->select(
+                'n.notation_id',
+                'n.user_id',
+                'n.name_notation',
+                'n.text_notation',
+                'users.name',
+                'users.avatar',
+                DB::raw("SUM(`nv`.`counter_views`) as counter_views"),
+                'n.notation_add_date as date_n',
+                'n.rating'
+            )
             ->join('users', 'users.id', '=', 'n.user_id')
             ->leftJoin('notation_views AS nv', 'n.notation_id', '=', 'nv.notation_id')
+            ->groupBy('nv.notation_id')
                 ->orderBy('notation_add_date', 'DESC')
             ->paginate(10)
             ->onEachSide(2);
